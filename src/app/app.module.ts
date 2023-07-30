564decaa8e063475f764ed4173d0e81a52bb65ab
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2023-07-30 17:39:30 +0400

    projectMade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CategoryComponent } from './category/category.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieComponent } from './movies/movie/movie.component';
+import { MovieDetailsComponent } from './movies/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CategoryComponent,
+    MoviesComponent,
+    MovieComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
